f5e389099c20636a9d28e8fbb63da81baf51fef6
Michael Rolli <michael.rolli@unibe.ch>
2025-09-06 06:52:50 +0200

    build: add env and tags to provisioning script

--- infra/provision.sh
+++ infra/provision.sh
@@ -2,15 +2,18 @@
 
 workload_name="hugo-static-app"
 region=westeurope
+environment="test"
+tags="environment=$environment division=id subDivision=idci managedBy=azcli"
 
-resource_group_name=$(printf "rg-%s" "$workload_name")
-static_web_app_name=$(printf "stapp-%s" "$workload_name")
+resource_group_name=$(printf "rg-%s-%s" "$workload_name" "$environment")
+static_web_app_name=$(printf "stapp-%s-%s" "$workload_name" "$environment")
 static_web_app_sku="Free"
 repo_url=$(git remote -v | awk '/push/{gsub(/\.git$/, "", $2); print $2}')
 
 az group create \
   --name "$resource_group_name" \
-  --location $region
+  --location $region \
+  --tags $tags
 
 az staticwebapp create \
   --name "$static_web_app_name" \
@@ -21,7 +24,8 @@ az staticwebapp create \
   --output-location "public" \
   --branch "main" \
   --sku "$static_web_app_sku" \
-  --login-with-github
+  --login-with-github \
+  --tags $tags
 
 webapp_url=$(az staticwebapp show --name "$static_web_app_name" --resource-group "$resource_group_name" --query "defaultHostname" -o tsv)
 
